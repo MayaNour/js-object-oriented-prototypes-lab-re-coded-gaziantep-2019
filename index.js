50a44d45ec91302b6a73a76ae93af10f2093da6d
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-15 21:28:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ function BoardMember(name, homeState, training){
      return "You will see great things from Scuber.";   }
 
    BoardMember.prototype.sayHi = function(){
-     return 'Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.'   }
+     return 'Hi, my name is ' + this.name +'. I am from ' + this.homeState+', and I was trained in ' + this.training +'.'   }
